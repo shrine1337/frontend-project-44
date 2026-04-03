62404e35ea5cb423c8e84ec7d0ecc11f8a3f773b
shrine1337 <nekitachuchundra228@gmail.com>
2026-04-03 11:56:29 +0700

    stage9-working

--- src/Games/prime.js
+++ src/Games/prime.js
@@ -3,8 +3,11 @@ import { mainName } from '../cli.js'
 
 export default () => {
   const name = mainName
+  const min = 0
+  const max = 100
+  let correctAnswer
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
-  for (let i = 0; i < 3; i++) {
-    
+  
   }
-}
+
