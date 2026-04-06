556609490b9f60507c7276fc1a623945287bf30a
shrine1337 <nekitachuchundra228@gmail.com>
2026-04-06 11:03:20 +0700

    fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import getName from '../src/cli.js'
-import even from '../src/Games/even.js'
+/* import even from '../src/Games/even.js' */
 
 function app() {
   getName()
-  even()
+ /*  even() */
 }
 app()
