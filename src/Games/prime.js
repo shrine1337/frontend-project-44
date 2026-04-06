e227cec86cde8c2f00fff0c3938dd90d47608303
shrine1337 <nekitachuchundra228@gmail.com>
2026-04-06 10:55:10 +0700

    stage9

--- src/Games/prime.js
+++ src/Games/prime.js
@@ -5,9 +5,35 @@ export default () => {
   const name = mainName
   const min = 0
   const max = 100
-  let correctAnswer
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+  let correctAnswer = 'yes'
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
-  
-  }
+  for (let i = 0; i < 3; i++) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+    if (randomNumber !== 2) {
+      if (randomNumber < 2 || randomNumber % 2 === 0) {
+        correctAnswer = 'no'
+      }
+      else if (randomNumber % 2 !== 0 && randomNumber >= 3) {
+        const sqrt = Math.floor(Math.sqrt(randomNumber))
+        for (let j = 3; j <= sqrt; j += 2) {
+          if (randomNumber % j === 0) {
+            correctAnswer = 'no'
+          }
+        }
+      }
+    }
+    console.log(`Question: ${randomNumber}`)
+    const firstAnswer = readlineSync.question(`Your answer: `)
+    if (firstAnswer === correctAnswer) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(`'${firstAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${name}!`)
+      break
+    }
 
+    if (i === 2) {
+      console.log(`Congratulations, ${name}`)
+    }
+  }
+}
