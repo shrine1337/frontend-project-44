1428150fd4e052ca29e787ad528df2b3090dec64
shrine1337 <nekitachuchundra228@gmail.com>
2026-04-08 09:55:17 +0700

    finalllllll

--- src/Games/even.js
+++ src/Games/even.js
@@ -5,10 +5,10 @@ export default () => {
   const name = mainName
   const min = 0
   const max = 100
-  let correctAnswer = 'yes'
   console.log('Answer "yes" if the number is even, otherwise answer "no".')
   for (let i = 0; i < 3; i++) {
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+    let correctAnswer = 'yes'
     if (randomNumber % 2 !== 0) {
       correctAnswer = 'no'
     }
